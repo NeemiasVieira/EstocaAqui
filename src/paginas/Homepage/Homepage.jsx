53e46c70930c62dea0205815724712b662580b79
Diogo Asenjo <diogoasenjo@outlook.com>
2023-12-14 16:32:48 -0300

    Finalizando a parte de entradas da homepage

--- src/paginas/Homepage/Homepage.jsx
+++ src/paginas/Homepage/Homepage.jsx
@@ -2,6 +2,8 @@ import { Navegacao } from "../../componentes/Navegacao/Navegacao";
 import { Footer } from "../../componentes/Footer/Footer";
 import Entrada from "../../componentes/Entrada/Entrada";
 import Saida from "../../componentes/Saida/Saida";
+import { EntradaService } from "../../serviços/API/modulos/EntradaService";
+import { useEffect, useState } from "react";
 
 const usuario = {
   nome: "Diogo",
@@ -9,70 +11,16 @@ const usuario = {
   outrasPropriedades: "desnecessárias",
 };
 
-const entradas = [
-  {
-    id: 8,
-    tipo: "Compra",
-    descricao: "Compra de novos materiais para o escritório",
-    nf: "32191105570714000825550010059146621133082968",
-    id_fornecedor: "5",
-    id_usuario: "13",
-    item: [
-      {
-        id_produto: 3,
-        quantidade: 5,
-      },
-      {
-        id_produto: 3,
-        quantidade: 7,
-      },
-    ],
-    createdAt: "2023-12-14T18:20:20.199Z",
-    updatedAt: "2023-12-14T18:20:20.199Z",
-  },
-  {
-    id: 9,
-    tipo: "Compra 2",
-    descricao: "Compra de novos materiais para o escritório",
-    nf: "32191105570714000825550010059146621133082968",
-    id_fornecedor: "5",
-    id_usuario: "13",
-    item: [
-      {
-        id_produto: 3,
-        quantidade: 5,
-      },
-      {
-        id_produto: 3,
-        quantidade: 7,
-      },
-    ],
-    createdAt: "2023-12-14T18:20:59.797Z",
-    updatedAt: "2023-12-14T18:20:59.797Z",
-  },
-  {
-    id: 10,
-    tipo: "Compra 3",
-    descricao: "Compra de novos materiais para o escritório",
-    nf: "32191105570714000825550010059146621133082968",
-    id_fornecedor: "5",
-    id_usuario: "13",
-    item: [
-      {
-        id_produto: 3,
-        quantidade: 5,
-      },
-      {
-        id_produto: 3,
-        quantidade: 7,
-      },
-    ],
-    createdAt: "2023-12-14T18:21:04.113Z",
-    updatedAt: "2023-12-14T18:21:04.113Z",
-  },
-];
-
 const Homepage = () => {
+  const [entradas, setEntrada] = useState();
+  const [erro, setErro] = useState();
+
+  const entradaService = new EntradaService(setEntrada, setErro);
+
+  useEffect(() => {
+    entradaService.getEntradas();
+  }, []);
+
   return (
     <>
       <Navegacao />
@@ -82,9 +30,14 @@ const Homepage = () => {
         <Saida />
 
         <p>Últimas entradas:</p>
-        {entradas.map((entrada) => (
-          <Entrada key={entrada.id} entrada={entrada} entradaCompleta={false} />
-        ))}
+        {entradas &&
+          entradas.map((entrada) => (
+            <Entrada
+              key={entrada.id}
+              entrada={entrada}
+              entradaCompleta={false}
+            />
+          ))}
       </section>
       <button>Estoque</button>
       <button>Entradas</button>
--- src/serviços/API/modulos/EntradaService.js
+++ src/serviços/API/modulos/EntradaService.js
@@ -9,10 +9,17 @@ export class EntradaService {
     this.setErro = setErro;
   }
 
-  async getEntradas(token) {
+  async getEntradas() {
+    const token = {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MDI1Nzc3MzUsImV4cCI6MTcwMjYwMjkzNSwic3ViIjoiMTMifQ.r8s7QUCHNu33JvjeQFDwfSChIoPIeC0e8zfBBPJIZ4c",
+      },
+    };
+
     await estoca_api
       .get("/entrada", token)
-      .then((resposta) => this.setEntrada(resposta))
+      .then((resposta) => this.setEntrada(resposta.data))
       .catch((erro) => this.setErro(erro));
   }
 }
